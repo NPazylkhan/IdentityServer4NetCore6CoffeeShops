5ab87d3c374a5723098975c2f814bda5816b6870
nurmakhan_pazylkhan <nurmakhan_pazylkhan@mail.ru>
2022-03-28 16:29:06 +0600

    first commit

---
+++ CoffeeShopper/DataAccess/Migrations/20220313124023_AddCoffeeShops.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class AddCoffeeShops : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"Insert INTO CoffeeShops(Name, OpeningHours, Address) Values(" +
+                $"'TopBarista', '9-5 Mon-Sat', 'Koktem 3')");
+            migrationBuilder.Sql($"Insert INTO CoffeeShops(Name, OpeningHours, Address) Values(" +
+                $"'StarBucks', '9-5 Mon-Sat', 'Al-Farabi 77')");
+            migrationBuilder.Sql($"Insert INTO CoffeeShops(Name, OpeningHours, Address) Values(" +
+                $"'Centeral Coffee', '9-5 Mon-Sat', 'Dostyq')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
